00b541f0dce5af82d5bc2a42d65a14f6532e82c5
Kishan Verma <kishanverma2312@gmail.com>
2022-07-07 20:35:45 +0530

    added dashboard and resume saving

--- models/db_models.js
+++ models/db_models.js
@@ -5,6 +5,8 @@ mongoose.connect('mongodb://localhost:27017/resumake').then(()=>{
     console.log('database connection successfull..');
 });
 
+
+//user schema
 const userSchema = mongoose.Schema({
     name:{
         type: String,
@@ -22,13 +24,38 @@ const userSchema = mongoose.Schema({
 });
 
 userSchema.pre('save',async function(next){
-    // if(this.password.isModified()){
-        let hash = await bcrypt.hash(this.password,10);
-        this.password = hash;
-    // }
+    let hash = await bcrypt.hash(this.password,10);
+    this.password = hash;
     next();
 });
 
 const User = mongoose.model('user',userSchema);
 
-module.exports = User;
+//resume schema
+const resumeSchema = mongoose.Schema({
+    name:{
+        type: String,
+        require:true
+    },
+
+    date:{
+        type: String,
+        require:true,
+        default: new Date().toISOString().slice(0, 10)
+    },
+    
+    user_mail:{
+        type:String,
+        require:true
+    },
+
+    data:{
+        type:String,
+        require:true
+    }
+});
+
+const Resume = mongoose.model('Resume',resumeSchema);
+
+//export
+module.exports = { User, Resume };
--- src/app.js
+++ src/app.js
@@ -7,11 +7,12 @@ const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const cookieParser = require("cookie-parser");
 const session = require('express-session');
+var flash = require('req-flash');
 
 require('dotenv').config({path: path.join(__dirname, '../.env')});
 
 const auth = require('./auth.js');
-const User = require('../models/users.js');
+const {User,Resume} = require('../models/db_models.js');
 const resutils = require('./resume_utils.js');
 
 const port = process.env.PORT || 8888;
@@ -31,7 +32,7 @@ app.use(session({
     saveUninitialized: true,
     // cookie: { secure: true }
 }));
-
+app.use(flash());
 
 //handlebar related
 app.set('view engine', 'hbs');
@@ -59,7 +60,7 @@ app.post('/login', async (req, res) => {
         if(valid){
             let token = await jwt.sign({id:data._id},process.env.SECRET_KEY);
             res.cookie('jwt',token);
-            // res.send(req.cookies.jwt);
+            req.session.user_mail = data.email;
             res.redirect('/dashboard');
         }
         else throw new Error('PAssword mismatch');
@@ -88,6 +89,7 @@ app.post('/signup', async (req, res) => {
                 let result = await thisUser.save();
                 let token = jwt.sign({id:result._id},process.env.SECRET_KEY);
                 res.cookie('jwt',token);
+                req.session.user_mail = req.body.email;
                 res.redirect('/dashboard');
             } catch (error) {
                 console.log(error);
@@ -106,19 +108,20 @@ app.post('/signup', async (req, res) => {
 
 app.get('/dashboard',async (req,res)=>{
     try {
-        
+        console.log(req.session);
         const jwt_cookie = req.cookies.jwt;
         if(jwt_cookie==null){
             res.redirect('/login');
-            // throw new Error('Please Login first');
         }
         else{
             let authorise = await auth(req.cookies.jwt);
-            // console.log(authorise);
             if(authorise.authorised){
-                let data  = await User.findOne({_id:authorise.id}).exec();
-                // console.log(data);
-                res.render('dashboard',{name:data.name, email:data.email});
+                let userData  = await User.findOne({_id:authorise.id}).exec();
+                req.session.user_mail = userData.email;
+                let resume_find_query = Resume.find({user_mail:userData.email});
+                resume_find_query.select('_id name date');
+                let resume_list = await resume_find_query.exec();
+                res.render('dashboard',{uname:userData.name, uemail:req.session.user_mail, resumes:resume_list});
             }
             else throw new Error(authorise.message);
         }
@@ -135,12 +138,24 @@ app.get('/create_resume',(req,res)=>{
 app.post('/create_resume',(req,res)=>{
     let obj = resutils(req.body);
     req.session.resume = obj;
-    res.redirect('/resume');
+    res.redirect('/preview_resume');
 });
 
-app.get('/resume',(req,res)=>{
+app.get('/preview_resume',(req,res)=>{
     res.render(`resume/${req.session.resume.resume_format}`,req.session.resume);
-    // res.send(req.session.resume);
+});
+
+app.get('/preview_resume/:id',async (req,res)=>{
+    try {
+        // console.log('here');
+        let result = await Resume.findOne({_id:req.params.id}).select('data').exec();
+        // console.log(result);
+        let resumeData = JSON.parse(result.data);
+        // console.log(resumeData.resume_format);
+        res.render(`resume/${resumeData.resume_format}`,resumeData);
+    } catch (error) {
+        res.status(400).send(error.message);
+    }
 });
 
 app.get('/logout',(req,res)=>{
@@ -151,6 +166,33 @@ app.get('/logout',(req,res)=>{
     res.redirect('/');
 })
 
+app.post('/save_resume',async (req,res)=>{
+    let resume_model = new Resume({
+        name: req.body.name,
+        user_mail: req.session.user_mail,
+        data: JSON.stringify(req.session.resume),
+    });
+
+    try {
+        let result = await resume_model.save();
+        // res.send('Succesfully saved');
+        req.flash('message','succesfully saved');
+        res.redirect('/dashboard');
+    } catch (error) {
+        res.status(400).send(error.message);
+    }
+});
+
+app.get('/delete/:id',async(req,res)=>{
+    try {
+        let result = await Resume.deleteOne({_id:req.params.id}).exec();
+        // console.log(result);
+        req.flash('message','successfully deleted');
+        res.redirect('/dashboard');
+    } catch (error) {
+        req.flash('message',error.message);
+    }
+})
 
 // //something necessary
 // hbs.registerHelper('exist', function (obj) {
